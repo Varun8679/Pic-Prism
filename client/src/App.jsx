86c7958f4114f8cee264013001aded85c59911cc
varun <varun123070@gmail.com>
2024-07-24 18:30:56 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import SellerDashboard from "./pages/SellerDashboard";
+import BuyerDashboard from "./pages/BuyerDashboard";
+import Navbar from "./componenets/Navbar";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/seller/profile" element={<SellerDashboard />} />
+          <Route path="/buyer/profile" element={<BuyerDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
